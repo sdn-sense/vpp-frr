c2a998693a2fe587745b7f123560969d465b9b43
Justas Balcas <juztas@gmail.com>
2024-10-10 09:48:54 -0500

    Update tuning.sh

--- tuning.sh
+++ tuning.sh
@@ -12,7 +12,7 @@ grep -q '^net.ipv4.tcp_adv_win_scale' /etc/sysctl.conf && sed -i 's/^net.ipv4.tc
 grep -q '^net.ipv4.tcp_low_latency' /etc/sysctl.conf && sed -i 's/^net.ipv4.tcp_low_latency.*/net.ipv4.tcp_low_latency=1/' /etc/sysctl.conf || echo 'net.ipv4.tcp_low_latency=1' >> /etc/sysctl.conf
 grep -q '^net.ipv4.tcp_timestamps' /etc/sysctl.conf && sed -i 's/^net.ipv4.tcp_timestamps.*/net.ipv4.tcp_timeip addressstamps=0/' /etc/sysctl.conf || echo 'net.ipv4.tcp_timestamps=0' >> /etc/sysctl.conf
 grep -q '^net.ipv4.tcp_sack' /etc/sysctl.conf && sed -i 's/^net.ipv4.tcp_sack.*/net.ipv4.tcp_sack=1/' /etc/sysctl.conf || echo 'net.ipv4.tcp_sack=1' >> /etc/sysctl.conf
-grep -q '^net.ipv4.tcp_moderate_rcvbuf ' /etc/sysctl.conf && sed -i 's/^net.ipv4.tcp_moderate_rcvbuf .*/net.ipv4.tcp_moderate_rcvbuf =1/' /etc/sysctl.conf || echo 'net.ipv4.tcp_moderate_rcvbuf =1' >> /etc/sysctl.conf
+grep -q '^net.ipv4.tcp_moderate_rcvbuf ' /etc/sysctl.conf && sed -i 's/^net.ipv4.tcp_moderate_rcvbuf .*/net.ipv4.tcp_moderate_rcvbuf=1/' /etc/sysctl.conf || echo 'net.ipv4.tcp_moderate_rcvbuf=1' >> /etc/sysctl.conf
 grep -q '^net.ipv4.tcp_congestion_control' /etc/sysctl.conf && sed -i 's/^net.ipv4.tcp_congestion_control.*/net.ipv4.tcp_congestion_control=bbr/' /etc/sysctl.conf || echo 'net.ipv4.tcp_congestion_control=bbr' >> /etc/sysctl.conf
 grep -q '^net.ipv4.tcp_mtu_probing' /etc/sysctl.conf && sed -i 's/^net.ipv4.tcp_mtu_probing.*/net.ipv4.tcp_mtu_probing=1/' /etc/sysctl.conf || echo 'net.ipv4.tcp_mtu_probing=1' >> /etc/sysctl.conf
 grep -q '^net.core.default_qdisc' /etc/sysctl.conf && sed -i 's/^net.core.default_qdisc.*/net.core.default_qdisc=fq/' /etc/sysctl.conf || echo 'net.core.default_qdisc=fq' >> /etc/sysctl.conf
