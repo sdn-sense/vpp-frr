250f05336eba8dd438cb16b36867c9bda5ce2c19
Justas Balcas <juztas@gmail.com>
2024-10-30 09:30:49 -0500

    Update vpp install

--- vpp/start.sh
+++ vpp/start.sh
@@ -1,3 +1,3 @@
-echo 4096 | sudo tee /proc/sys/vm/nr_hugepages
-docker build --network host . -t vpp
+echo 65536 | sudo tee /proc/sys/vm/nr_hugepages
+docker build --network host -t vpp .
 docker compose up -d
