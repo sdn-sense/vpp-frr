430472519ceb4d83270131a10a6dd04153951d74
Justas Balcas <juztas@gmail.com>
2024-11-06 16:15:43 -0600

    Allow to configure multiple interfaces

--- vpp/run.sh
+++ vpp/run.sh
@@ -3,45 +3,43 @@
 CONFIG_TEMPLATE="/etc/vpp/startup.conf-template"
 CONFIG_OUTPUT="/etc/vpp/startup.conf"
 
+# Function to configure interface
+configure_interface() {
+    local interface=$1
+    # Check if interface variable is defined and not empty
+    if [ -z "$interface" ]; then
+        echo "Skipping undefined interface variable."
+        return
+    fi
 
-# Set interfaces down (vpp expects them to be down as dpdk will overtake it)
-ip link set down $ENV_PUBLIC_INTF
-# Hardware specific Optimizations for public intf
-ethtool -K $ENV_PUBLIC_INTF rx off
-ethtool -K $ENV_PUBLIC_INTF tx off
-ethtool -K $ENV_PUBLIC_INTF sg off
-ethtool -K $ENV_PUBLIC_INTF tso off
-ethtool -K $ENV_PUBLIC_INTF ufo off
-ethtool -K $ENV_PUBLIC_INTF gso off
-ethtool -K $ENV_PUBLIC_INTF gro off
-ethtool -K $ENV_PUBLIC_INTF lro off
-ethtool -K $ENV_PUBLIC_INTF rxvlan off
-ethtool -K $ENV_PUBLIC_INTF txvlan off
-ethtool -K $ENV_PUBLIC_INTF ntuple off
-ethtool -K $ENV_PUBLIC_INTF rxhash off
-ethtool --set-eee $ENV_PUBLIC_INTF eee off
+    # Set interface down
+    ip link set down "$interface"
+    # Apply hardware-specific optimizations
+    ethtool -K "$interface" rx off
+    ethtool -K "$interface" tx off
+    ethtool -K "$interface" sg off
+    ethtool -K "$interface" tso off
+    ethtool -K "$interface" ufo off
+    ethtool -K "$interface" gso off
+    ethtool -K "$interface" gro off
+    ethtool -K "$interface" lro off
+    ethtool -K "$interface" rxvlan off
+    ethtool -K "$interface" txvlan off
+    ethtool -K "$interface" ntuple off
+    ethtool -K "$interface" rxhash off
+    ethtool --set-eee "$interface" eee off
+}
 
-# Set private interface down (vpp will start it and expects it to be down)
-ip link set down $ENV_PRIVATE_INTF
-# Hardware specific Optimizations for public intf
-ethtool -K $ENV_PRIVATE_INTF rx off
-ethtool -K $ENV_PRIVATE_INTF tx off
-ethtool -K $ENV_PRIVATE_INTF sg off
-ethtool -K $ENV_PRIVATE_INTF tso off
-ethtool -K $ENV_PRIVATE_INTF ufo off
-ethtool -K $ENV_PRIVATE_INTF gso off
-ethtool -K $ENV_PRIVATE_INTF gro off
-ethtool -K $ENV_PRIVATE_INTF lro off
-ethtool -K $ENV_PRIVATE_INTF rxvlan off
-ethtool -K $ENV_PRIVATE_INTF txvlan off
-ethtool -K $ENV_PRIVATE_INTF ntuple off
-ethtool -K $ENV_PRIVATE_INTF rxhash off
-ethtool --set-eee $ENV_PRIVATE_INTF eee off
+# Array of interfaces to configure
+interfaces=("$ENV_PUBLIC_INTF" "$ENV_PUBLIC1_INTF" "$ENV_PUBLIC2_INTF" "$ENV_PRIVATE_INTF")
 
+# Loop through each interface and configure it
+for interface in "${interfaces[@]}"; do
+    configure_interface "$interface"
+done
 
 # Update configuration file to match correct PCI device inside VPP
-
-cp $CONFIG_TEMPLATE $CONFIG_OUTPUT
+cp "$CONFIG_TEMPLATE" "$CONFIG_OUTPUT"
 
 # Replace all variables in the configuration file
 sed -i "s/ENV_MAIN_CORE/$ENV_MAIN_CORE/g" "$CONFIG_OUTPUT"
@@ -56,12 +54,13 @@ sed -i "s/ENV_PUBLIC_INTF_TXQ/$ENV_PUBLIC_INTF_TXQ/g" "$CONFIG_OUTPUT"
 sed -i "s/ENV_PUBLIC_INTF_RXDESC/$ENV_PUBLIC_INTF_RXDESC/g" "$CONFIG_OUTPUT"
 sed -i "s/ENV_PUBLIC_INTF_TXDESC/$ENV_PUBLIC_INTF_TXDESC/g" "$CONFIG_OUTPUT"
 sed -i "s/ENV_PUBLIC_INTF_PCI/$ENV_PUBLIC_INTF_PCI/g" "$CONFIG_OUTPUT"
-sed -i "s/ENV_PRIVATE_INTF_PCI/$ENV_PRIVATE_INTF_PCI/g" "$CONFIG_OUTPUT"
+sed -i "s/ENV_PUBLIC1_INTF_PCI/$ENV_PUBLIC1_INTF_PCI/g" "$CONFIG_OUTPUT"
+sed -i "s/ENV_PUBLIC2_INTF_PCI/$ENV_PUBLIC2_INTF_PCI/g" "$CONFIG_OUTPUT"
 sed -i "s/ENV_PUBLIC_INTF/$ENV_PUBLIC_INTF/g" "$CONFIG_OUTPUT"
 sed -i "s/ENV_PRIVATE_INTF/$ENV_PRIVATE_INTF/g" "$CONFIG_OUTPUT"
 
 echo "Configuration generated at $CONFIG_OUTPUT"
-cat $CONFIG_OUTPUT
+cat "$CONFIG_OUTPUT"
 
 echo "Start VPP"
 exec /usr/bin/vpp -c /etc/vpp/startup.conf
